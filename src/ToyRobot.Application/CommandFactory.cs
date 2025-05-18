c5a28ce4b7fe68a06c1fbacc4a38596033cf2db2
kiran <kiran>
2025-05-18 13:25:45 +1000

    Add command factory

---
+++ src/ToyRobot.Application/CommandFactory.cs
@@ -0,0 +1,37 @@
+using ToyRobot.Application.Commands;
+using ToyRobot.Domain;
+using System;
+
+namespace ToyRobot.Application;
+
+public class CommandFactory(Robot robot)
+{
+    public ICommand Create(ParsedCommand parsedCommand)
+    {
+        
+        return parsedCommand.CommandType switch
+        {
+            CommandType.Place => CreatePlaceCommand(parsedCommand.RawArgs),
+            CommandType.Move => new MoveCommand(robot),
+            CommandType.Left => new LeftCommand(robot),
+            CommandType.Right => new RightCommand(robot),
+            CommandType.Report => new ReportCommand(robot),
+            _ => new NoOpCommand()
+        };
+    }
+
+    private ICommand CreatePlaceCommand(string rawArgs)
+    {
+        var args = rawArgs.Split(',');
+
+        if (args.Length != 3 ||
+            !int.TryParse(args[0], out var x) ||
+            !int.TryParse(args[1], out var y) ||
+            !Enum.TryParse<Direction>(args[2], true, out var direction))
+        {
+            return new NoOpCommand();
+        }
+
+        return new PlaceCommand(robot, x, y, direction);
+    }
+}
