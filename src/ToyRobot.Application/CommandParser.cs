5dd55ba1faa8fc595ae07e757295e1eb0b042e7c
kiran <kiran>
2025-05-18 13:17:36 +1000

    Add CommandParser

---
+++ src/ToyRobot.Application/CommandParser.cs
@@ -0,0 +1,34 @@
+namespace ToyRobot.Application;
+
+public class CommandParser
+{
+    public ParsedCommand Parse(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input)) 
+            return ParsedCommand.Invalid;
+        
+        var inputParts = input.Trim().Split(' ', 2);
+        var commandText = inputParts[0];
+        var rawArgs = inputParts.Length > 1 ? inputParts[1] : string.Empty;
+
+        if (!Enum.TryParse<CommandType>(commandText, ignoreCase: true, out var commandType))
+            commandType = CommandType.Invalid;
+
+        return new ParsedCommand(commandType, rawArgs);
+    }
+}
+
+public readonly record struct ParsedCommand(CommandType CommandType, string RawInput = "")
+{
+    public static ParsedCommand Invalid => new(CommandType.Invalid);
+}
+
+public enum CommandType
+{
+    Place,
+    Move,
+    Left,
+    Right,
+    Report,
+    Invalid
+}
