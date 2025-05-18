b2f8649914b87728eb26b920be68b88d46daa27a
kiran <kiran>
2025-05-18 13:29:19 +1000

    Add console app

---
+++ src/ToyRobot.Console/Program.cs
@@ -0,0 +1,34 @@
+using ToyRobot.Application;
+using ToyRobot.Domain;
+
+namespace ToyRobot.Console;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Toy Robot Simulator");
+        System.Console.WriteLine("Enter commands (PLACE 0,0,NORTH | MOVE | LEFT | RIGHT)");
+
+        var table = new Table();
+        var robot = new Robot(table);
+        var commandParser = new CommandParser();
+        var commandFactory = new CommandFactory(robot);
+
+        while (true)
+        {
+            var commandText = System.Console.ReadLine();
+            if (commandText == null)
+            {
+                break;
+            }
+
+            var result = commandParser.Parse(commandText);
+            var command = commandFactory.Create(result);
+            command.Execute();
+            
+            if (result.CommandType == CommandType.Report)
+                break;
+        }
+    }
+}
